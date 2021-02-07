aac19b59d6634c477f45568b0d5a4ff408bfc1cd
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2021-02-07 17:32:57 +0100

    Add files via upload

---
+++ svanTipuricKlaseIObjektiTreciZad/svanTipuricKlaseIObjektiTreciZad/KlasaX.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricKlaseIObjektiTreciZad
+{
+    class KlasaX
+    {
+       
+        static int i= 100;
+        public int[] arr = new int[i];
+        
+        
+            
+        
+        
+         
+    }
+}
---
+++ svanTipuricKlaseIObjektiTreciZad/svanTipuricKlaseIObjektiTreciZad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricKlaseIObjektiTreciZad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX x = new KlasaX();
+
+            for (int i = 0; i < arr.lenght(); i++)
+            {
+                Console.WriteLine("Upisite" + i + " clan niza");
+                arr[] = Console.ReadLine()
+            }
+            for (int i = 0; i < arr.lenght(); i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadKey()
+        }
+    }
+}
